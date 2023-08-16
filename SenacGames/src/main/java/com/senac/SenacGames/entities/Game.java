cb8e3023f235079af554864bba52c0c2e993d2c9
igor47g0 <Edihigor17@gmail.com>
2023-08-15 21:41:30 -0300

    ajustes

--- SenacGames/src/main/java/com/senac/SenacGames/entities/Game.java
+++ SenacGames/src/main/java/com/senac/SenacGames/entities/Game.java
@@ -1,6 +1,12 @@
 package com.senac.SenacGames.entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String genero;
